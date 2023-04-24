a024870f7d625e0eb1b0386ab75d49b9b13e71a3
rwlipunisher <129790481+rwlipunisher@users.noreply.github.com>
2023-04-24 00:20:04 -0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,19 @@
+from jira import JIRA
+from dotenv import load_dotenv
+import requests
+import os
+
+
+def authenticator():
+  '''Retorna o objeto jira para requestes na conta do usuario.'''
+    load_dotenv()
+    apikey = os.environ['jira_token']
+    user = 'usuario de acesso ao jira'
+    server = 'https://seudominojira.atlassian.net'
+    options = { 'server': server }
+
+    jira = JIRA(options, basic_auth=(user, apikey))
+
+    return jira
+
+
